7c3e1bc3480b90ad485f5fe66094716069f0c645
Art2485 <ooartyguitaroo@gmail.com>
2025-08-13 20:56:04 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
@@ -12,5 +12,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "RecoverEasy9" // ชื่ออะไรก็ได้
+
+rootProject.name = "RecoverEasy9"
 include(":app")
