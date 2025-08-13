e655fe5a19ef096b8d00c895cf61c8fa1537a0bc
Art2485 <ooartyguitaroo@gmail.com>
2025-08-13 11:30:56 +0700

    Update MediaItem.kt

--- app/src/main/java/com/recovereasy/MediaItem.kt
+++ app/src/main/java/com/recovereasy/MediaItem.kt
@@ -2,13 +2,13 @@ package com.recovereasy
 
 import android.net.Uri
 
-enum class MediaKind { IMAGE, VIDEO, AUDIO, OTHER }
-
 data class MediaItem(
-  val uri: Uri,
-  val name: String,
-  val size: Long,
-  val kind: MediaKind,
-  val sourceLabel: String,   // phone / otg / folder
-  val suspectCorrupted: Boolean = false
+    val uri: Uri,
+    val name: String,
+    val mime: String,
+    val size: Long,
+    val date: Long,
+    val durationMs: Long? = null,
+    val isTrashed: Boolean = false,
+    val isCorrupted: Boolean = false
 )
